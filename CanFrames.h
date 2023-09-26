12e35c7aa648addbecb0d56a6d160f741c20823b
kezcelm <bartekmleczek@gmail.com>
2023-09-26 19:14:35 +0200

    + CanFrames.h

---
+++ CanFrames.h
@@ -0,0 +1,24 @@
+#ifndef CAN_FRAMES_H
+#define CAN_FRAMES_H
+
+#include <Arduino.h>
+
+struct can_frame frame580;
+struct can_frame frame581;
+struct can_frame frame582;
+struct can_frame frame583;
+struct can_frame frame584;
+struct can_frame frame590;
+struct can_frame frame591;
+struct can_frame frame592;
+struct can_frame frame593;
+struct can_frame frame594;
+struct can_frame frame595;
+struct can_frame frame59F;
+struct can_frame frame780;
+struct can_frame frame781;
+struct can_frame frame782;
+struct can_frame frame783;
+struct can_frame frame784;
+
+#endif
