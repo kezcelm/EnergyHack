8b04ba82b8c5f73f4871cbc6a13cfebef2465076
kezcelm <bartekmleczek@gmail.com>
2023-10-26 14:38:56 +0200

    .

--- EnergyHack/CanFrames.h
+++ EnergyHack/CanFrames.h
@@ -3,13 +3,21 @@
 
 #include <Arduino.h>
 
+struct can_frame {
+    unsigned long can_id;
+    byte can_ext;
+    byte can_dlc;
+    const byte *data; 
+    bool wait_sent=true;
+};
+
 struct can_frame frame580;
-__u8 data580[] = {0x00, 
+unsigned char data580[] = {0x00, 
                   0xC0, 
                   0x00};
 
 struct can_frame frame581;
-__u8 data581[] = {0x00, // Battery level - data[0] = 0x64 - 100%
+unsigned char data581[] = {0x00, // Battery level - data[0] = 0x64 - 100%
                   0x95, 
                   0x37, 
                   0xE3, 
@@ -18,30 +26,30 @@ __u8 data581[] = {0x00, // Battery level - data[0] = 0x64 - 100%
                   0x42}; 
 
 struct can_frame frame582;
-__u8 data582[] = {0x00, 
+unsigned char data582[] = {0x00, 
                   0x00, 
                   0x78, 
                   0x9E};
 
 struct can_frame frame583;
-__u8 data583[] = {0xF4, 
+unsigned char data583[] = {0xF4, 
                   0x01, 
                   0x48, 
                   0xA3, 
                   0x01};
 
 struct can_frame frame584;
-__u8 data584[] = {0x00, 
+unsigned char data584[] = {0x00, 
                   0x00, 
                   0x00, 
                   0x00};
 
 struct can_frame frame590;
-__u8 data590[] = {0x0B, 
+unsigned char data590[] = {0x0B, 
                   0x14};
 
 struct can_frame frame591;
-__u8 data591[] = {0x14, 
+unsigned char data591[] = {0x14, 
                   0x0A, 
                   0x13, 
                   0xB0, 
@@ -51,7 +59,7 @@ __u8 data591[] = {0x14,
                   0x00};
 
 struct can_frame frame592;
-__u8 data592[] = {0x1D, 
+unsigned char data592[] = {0x1D, 
                   0x10, 
                   0x1A, 
                   0x10, 
@@ -59,7 +67,7 @@ __u8 data592[] = {0x1D,
                   0x93};
 
 struct can_frame frame593;
-__u8 data593[] = {0x20, 
+unsigned char data593[] = {0x20, 
                   0x00, 
                   0x15, 
                   0x00, 
@@ -69,13 +77,13 @@ __u8 data593[] = {0x20,
                   0x5E};
 
 struct can_frame frame594;
-__u8 data594[] = {0x28, 
+unsigned char data594[] = {0x28, 
                   0x27, 
                   0x08, 
                   0xF4};
 
 struct can_frame frame595;
-__u8 data595[] = {0x61, 
+unsigned char data595[] = {0x61, 
                   0x10, 
                   0xB0, 
                   0x0A, 
@@ -83,15 +91,15 @@ __u8 data595[] = {0x61,
                   0x79};
 
 struct can_frame frame59F;
-__u8 data59F[] = {0x01}; // data[0] = 0x00 or 0x01
+unsigned char data59F[] = {0x01}; // data[0] = 0x00 or 0x01
 
 struct can_frame frame780;
-__u8 data780[] = {0x00, 
+unsigned char data780[] = {0x00, 
                   0xC0, 
                   0x00};
 
 struct can_frame frame781;
-__u8 data781[] = {0x00,  // Battery level - data[0] = 0x64 - 100%
+unsigned char data781[] = {0x00,  // Battery level - data[0] = 0x64 - 100%
                   0x95, 
                   0x37, 
                   0xE3, 
@@ -100,20 +108,20 @@ __u8 data781[] = {0x00,  // Battery level - data[0] = 0x64 - 100%
                   0x42};
 
 struct can_frame frame782;
-__u8 data782[] = {0x21, 
+unsigned char data782[] = {0x21, 
                   0x00, 
                   0x7A, 
                   0x9E};
 
 struct can_frame frame783;
-__u8 data783[] = {0xA0, 
+unsigned char data783[] = {0xA0, 
                   0x0F, 
                   0x48, 
                   0xA3, 
                   0x01};
 
 struct can_frame frame784;
-__u8 data784[] = {0x00, 
+unsigned char data784[] = {0x00, 
                   0x0C, 
                   0x00};
 
--- SendCanFrames/CanFrames.h
+++ SendCanFrames/CanFrames.h
@@ -3,21 +3,13 @@
 
 #include <Arduino.h>
 
-struct can_frame {
-    unsigned long can_id;
-    byte can_ext;
-    byte can_dlc;
-    const byte *data; 
-    bool wait_sent=true;
-};
-
 struct can_frame frame580;
-unsigned char data580[] = {0x00, 
+__u8 data580[] = {0x00, 
                   0xC0, 
                   0x00};
 
 struct can_frame frame581;
-unsigned char data581[] = {0x00, // Battery level - data[0] = 0x64 - 100%
+__u8 data581[] = {0x00, // Battery level - data[0] = 0x64 - 100%
                   0x95, 
                   0x37, 
                   0xE3, 
@@ -26,30 +18,30 @@ unsigned char data581[] = {0x00, // Battery level - data[0] = 0x64 - 100%
                   0x42}; 
 
 struct can_frame frame582;
-unsigned char data582[] = {0x00, 
+__u8 data582[] = {0x00, 
                   0x00, 
                   0x78, 
                   0x9E};
 
 struct can_frame frame583;
-unsigned char data583[] = {0xF4, 
+__u8 data583[] = {0xF4, 
                   0x01, 
                   0x48, 
                   0xA3, 
                   0x01};
 
 struct can_frame frame584;
-unsigned char data584[] = {0x00, 
+__u8 data584[] = {0x00, 
                   0x00, 
                   0x00, 
                   0x00};
 
 struct can_frame frame590;
-unsigned char data590[] = {0x0B, 
+__u8 data590[] = {0x0B, 
                   0x14};
 
 struct can_frame frame591;
-unsigned char data591[] = {0x14, 
+__u8 data591[] = {0x14, 
                   0x0A, 
                   0x13, 
                   0xB0, 
@@ -59,7 +51,7 @@ unsigned char data591[] = {0x14,
                   0x00};
 
 struct can_frame frame592;
-unsigned char data592[] = {0x1D, 
+__u8 data592[] = {0x1D, 
                   0x10, 
                   0x1A, 
                   0x10, 
@@ -67,7 +59,7 @@ unsigned char data592[] = {0x1D,
                   0x93};
 
 struct can_frame frame593;
-unsigned char data593[] = {0x20, 
+__u8 data593[] = {0x20, 
                   0x00, 
                   0x15, 
                   0x00, 
@@ -77,13 +69,13 @@ unsigned char data593[] = {0x20,
                   0x5E};
 
 struct can_frame frame594;
-unsigned char data594[] = {0x28, 
+__u8 data594[] = {0x28, 
                   0x27, 
                   0x08, 
                   0xF4};
 
 struct can_frame frame595;
-unsigned char data595[] = {0x61, 
+__u8 data595[] = {0x61, 
                   0x10, 
                   0xB0, 
                   0x0A, 
@@ -91,15 +83,15 @@ unsigned char data595[] = {0x61,
                   0x79};
 
 struct can_frame frame59F;
-unsigned char data59F[] = {0x01}; // data[0] = 0x00 or 0x01
+__u8 data59F[] = {0x01}; // data[0] = 0x00 or 0x01
 
 struct can_frame frame780;
-unsigned char data780[] = {0x00, 
+__u8 data780[] = {0x00, 
                   0xC0, 
                   0x00};
 
 struct can_frame frame781;
-unsigned char data781[] = {0x00,  // Battery level - data[0] = 0x64 - 100%
+__u8 data781[] = {0x00,  // Battery level - data[0] = 0x64 - 100%
                   0x95, 
                   0x37, 
                   0xE3, 
@@ -108,20 +100,20 @@ unsigned char data781[] = {0x00,  // Battery level - data[0] = 0x64 - 100%
                   0x42};
 
 struct can_frame frame782;
-unsigned char data782[] = {0x21, 
+__u8 data782[] = {0x21, 
                   0x00, 
                   0x7A, 
                   0x9E};
 
 struct can_frame frame783;
-unsigned char data783[] = {0xA0, 
+__u8 data783[] = {0xA0, 
                   0x0F, 
                   0x48, 
                   0xA3, 
                   0x01};
 
 struct can_frame frame784;
-unsigned char data784[] = {0x00, 
+__u8 data784[] = {0x00, 
                   0x0C, 
                   0x00};
 
